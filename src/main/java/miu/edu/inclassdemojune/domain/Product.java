b297371f46db8ac7e719e3aeb86064d47f0c20f5
Muhyieddin <muhyidean@gmail.com>
2023-05-31 13:26:16 -0500

    removed the category property from the Product

--- src/main/java/miu/edu/inclassdemojune/domain/Product.java
+++ src/main/java/miu/edu/inclassdemojune/domain/Product.java
@@ -17,7 +17,6 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String category;
     private int starCount;
     private double price;
 
